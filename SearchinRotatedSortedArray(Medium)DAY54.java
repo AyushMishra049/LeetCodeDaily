99d84ff1ac8671ec19ec2fe79cd8946b7b224c00
AyushMishra049 <mishraayush049@gmail.com>
2026-02-25 12:40:32 +0530

    DAY 54 DONE

---
+++ SearchinRotatedSortedArray(Medium)DAY54.java
@@ -0,0 +1,33 @@
+//SearchinRotatedSortedArray(Medium)DAY54 
+
+class Solution {
+    public int search(int[] nums, int target) {
+
+        int i=0;
+        int j=nums.length-1;
+        while(i<=j){
+            int m=i+(j-i)/2;
+            if(nums[m]==target){
+                return m;
+            }
+            if(nums[i]<=nums[m]){
+                if(nums[i]<=target && target<=nums[m]){
+                    j=m-1;                }
+                    else{
+                        i=m+1;
+                    }
+            }
+            else{
+                if(nums[m]<=target && target <=nums[j]){
+                    i=m+1;
+                }
+                else{
+                    j=m-1;
+                }
+            }
+
+        }
+        return -1;
+        
+    }
+}
